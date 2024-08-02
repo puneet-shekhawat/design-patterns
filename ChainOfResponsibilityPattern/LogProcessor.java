1ddd2474bf30a611fdc8fd54d8cdfaf116828db5
Puneet Shekhawat <puneetshekhawat1998@gmail.com>
2024-08-02 22:26:10 +0530

    chain of responsibility design pattern added

---
+++ ChainOfResponsibilityPattern/LogProcessor.java
@@ -0,0 +1,18 @@
+package ChainOfResponsibilityPattern;
+
+public abstract class LogProcessor {
+    public static int INFO = 1;
+    public static int ERROR = 2;
+    public static int DEBUG = 3;
+    LogProcessor nextLogProcessor;
+
+    LogProcessor(LogProcessor nextLogProcessor) {
+        this.nextLogProcessor = nextLogProcessor;
+    }
+
+    public void log(int logLevel, String message) {
+        if(nextLogProcessor != null)
+            nextLogProcessor.log(logLevel, message);
+    }
+
+}
